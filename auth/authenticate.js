c70a6ef1a8e0a3d644ae0153160bbe3de168d726
ruihildt <ruihildt@protonmail.com>
2019-07-26 10:59:50 +0200

    Add token generation and register function

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const jwtKey = process.env.JWT_SECRET;
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
 
 // implementation details
@@ -26,4 +25,16 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
+}
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1hr'
+  };
+
+  return jwt.sign(payload, jwtKey, options);
 }
