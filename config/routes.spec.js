5f7a8f3b7ff363d7729972d43d291346c2d00590
ruihildt <ruihildt@protonmail.com>
2019-07-26 12:09:01 +0200

    Add first test

--- config/routes.spec.js
+++ config/routes.spec.js
@@ -1,1 +1,34 @@
-const db = require('../../data/dbConfig');
+const request = require("supertest");
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+beforeEach(async () => {
+	await db('users').truncate();
+});
+
+describe('[POST] /api/register endpoint', () => {
+	it('adds a new user to the database', () => {
+		return request(server)
+		.post('/api/register')
+		.send({ username: 'One True Gabe', password: '123456'})
+		.expect(201)
+		.then(res => {
+			expect(res.body.id).toBe(1);
+		})
+
+	});
+});
+
+describe('[POST] /api/login endpoint', () => {
+
+	beforeEach(async () => {
+		return request(server)
+		.post('/api/auth/register')
+		.send({
+			username: 'DangerMan',
+			password: '123456',
+		})
+		.expect(201);
+	});
+	
+});
