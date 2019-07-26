7089b342c26cb87249e477cf3e19485dbeae0601
ruihildt <ruihildt@protonmail.com>
2019-07-26 12:15:11 +0200

    Add second test

--- config/routes.spec.js
+++ config/routes.spec.js
@@ -10,7 +10,9 @@ describe('[POST] /api/register endpoint', () => {
 	it('adds a new user to the database', () => {
 		return request(server)
 		.post('/api/register')
-		.send({ username: 'One True Gabe', password: '123456'})
+		.send({ 
+			username: 'One True Gabe', 
+			password: '123456'})
 		.expect(201)
 		.then(res => {
 			expect(res.body.id).toBe(1);
@@ -21,14 +23,26 @@ describe('[POST] /api/register endpoint', () => {
 
 describe('[POST] /api/login endpoint', () => {
 
-	beforeEach(async () => {
+	beforeEach( () => {
 		return request(server)
-		.post('/api/auth/register')
+		.post('/api/register')
 		.send({
 			username: 'DangerMan',
 			password: '123456',
 		})
 		.expect(201);
 	});
-	
+
+	it('is able to login with user ', () => {
+		return request(server)
+			.post('/api/login')
+			.send({
+				username: 'DangerMan',
+				password: '123456',
+			})
+			.expect(200)
+			.then(res => {
+				expect(res.body.message).toEqual('Welcome DangerMan!');
+			});
+	});
 });
